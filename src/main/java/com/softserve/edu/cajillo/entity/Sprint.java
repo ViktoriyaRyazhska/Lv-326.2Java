9800ad1a65301723f5050233ee4740382c0a674a
marisha92 <novitska.mv@gmail.com>
2018-07-27 15:51:47 +0300

    Backlog and Scrum Entity added

---
+++ src/main/java/com/softserve/edu/cajillo/entity/Sprint.java
@@ -0,0 +1,34 @@
+package com.softserve.edu.cajillo.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "sprints")
+public class Sprint {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String label;
+
+    private Instant startDate;
+    private Instant endDate;
+
+    //long description about the goal of sprint (length 200)
+    private String goal;
+
+    @OneToMany(fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL,
+            mappedBy = "ticket")
+    private List<Ticket> tickets = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY,
+            cascade = CascadeType.ALL)
+    private Backlog backlog;
+}
