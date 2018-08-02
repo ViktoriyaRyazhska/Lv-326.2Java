0807e75303397bfe9d48fad1b2b3333f6f04dee8
tonikromik <antonov.roman.o@gmail.com>
2018-08-02 14:02:50 +0300

    Added creating of team

--- src/main/java/com/softserve/edu/cajillo/converter/impl/RoleManagerConverterImpl.java
+++ src/main/java/com/softserve/edu/cajillo/converter/impl/RoleManagerConverterImpl.java
@@ -3,21 +3,37 @@ package com.softserve.edu.cajillo.converter.impl;
 import com.softserve.edu.cajillo.converter.RoleManagerConverter;
 import com.softserve.edu.cajillo.dto.RoleManagerDto;
 import com.softserve.edu.cajillo.entity.RoleManager;
+import com.softserve.edu.cajillo.service.TeamService;
+import com.softserve.edu.cajillo.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RoleManagerConverterImpl implements RoleManagerConverter {
 
     @Autowired
     private ModelMapper modelMapper;
 
+    @Autowired
+    private TeamService teamService;
+
+    @Autowired
+    private UserService userService;
+
     @Override
     public RoleManager convertToEntity(RoleManagerDto dto) {
-        return modelMapper.map(dto, RoleManager.class);
+        RoleManager map = modelMapper.map(dto, RoleManager.class);
+        map.setTeam(teamService.getTeam(dto.getTeam_id()));
+        map.setUser(userService.getUser(dto.getUser_id()));
+        return map;
     }
 
     @Override
     public RoleManagerDto convertToDto(RoleManager entity) {
-        return modelMapper.map(entity, RoleManagerDto.class);
+        RoleManagerDto map = modelMapper.map(entity, RoleManagerDto.class);
+        map.setTeam_id(entity.getTeam().getId());
+        map.setUser_id(entity.getUser().getId());
+        return map;
     }
 }
--- src/main/java/com/softserve/edu/cajillo/service/TeamService.java
+++ src/main/java/com/softserve/edu/cajillo/service/TeamService.java
@@ -2,10 +2,11 @@ package com.softserve.edu.cajillo.service;
 
 import com.softserve.edu.cajillo.dto.TeamDto;
 import com.softserve.edu.cajillo.entity.Team;
+import com.softserve.edu.cajillo.security.UserPrincipal;
 
 public interface TeamService {
 
     Team getTeam(Long id);
 
-    Team createTeam(TeamDto teamDto);
+    void createTeam(TeamDto teamDto, UserPrincipal currentUser);
 }
